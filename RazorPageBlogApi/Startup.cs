4993efe6f9ee7516d295ac0a030fbe3b65bb23a2
林宏宇 <k86gundam@gmail.com>
2020-06-12 15:14:11 +0800

    加入資料庫&連線字串

--- RazorPageBlogApi/Startup.cs
+++ RazorPageBlogApi/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using RazorPageBlogApi.Data;
 
 namespace RazorPageBlogApi
 {
@@ -26,6 +22,13 @@ namespace RazorPageBlogApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<RazorPageBlogDbContext>(
+                options =>
+                    options.UseSqlServer(
+                        Configuration
+                            .GetConnectionString("DefaultConnection")));
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
